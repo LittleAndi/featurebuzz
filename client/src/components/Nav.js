60be513366ce00aa617d8c2f55df2f417a835135
Lars Nyström <nystrom.lasse@gmail.com>
2023-03-25 23:18:13 +0100

    Change navbar styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 const Nav = () => {
   return (
     <nav className="navbar">
       <h3>FeatureBuzz</h3>
       <div className="nav__BtnGroup">
-        <Link to="/ideas">All Ideas</Link>
-        <Link to="/user/ideas">My Ideas</Link>
+        <Link to="/">All Ideas</Link>
+        {/* <Link to="/user/ideas">My Ideas</Link> */}
         <Link to="/createidea">Create Idea</Link>
       </div>
     </nav>
